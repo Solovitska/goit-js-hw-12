e22cbc64d0833e3c1b432951852f4e0ed4e40b9a
Solovitska <146887721+Solovitska@users.noreply.github.com>
2024-02-29 22:50:29 +0200

    up-src

---
+++ src/js/render-functions.js
@@ -0,0 +1,25 @@
+import iziToast from 'izitoast';
+
+export function displayErrorToast() {
+  iziToast.error({
+    title: 'Error',
+    message: 'An error occurred. Please try again later.',
+    messageColor: '#FFF',
+    color: '#EF4040',
+    position: 'topRight',
+    timeout: 3000,
+  });
+}
+
+export function displayNoResultsToast() {
+  iziToast.show({
+    message: "Sorry, there are no images matching your search query. Please try again!",
+    messageColor: '#FFF',
+    color: '#EF4040',
+    position: 'topRight',
+    timeout: 3000,
+    messageSize: '16px',
+    messageLineHeight: '24px',
+    maxWidth: '432px',
+  });
+}
